b59bc0650dc31f2a938e25e3c972f0c3f1ce97b0
shunsuke-kawata <kwt20238120@gmail.com>
2025-04-29 17:46:53 +0900

    環境変数の更新

--- frontend_react/src/components/calender/calender.tsx
+++ frontend_react/src/components/calender/calender.tsx
@@ -1,7 +1,17 @@
-import { GOOGLE_CALENDER_API_KEY,GOOGLE_CALENDER_ID } from "../../config";
+import { GOOGLE_CALENDER_API_KEY, GOOGLE_CALENDER_ID } from "../../config";
 
-const Calender:React.FC = ()=>{
-    return <><iframe src="https://calendar.google.com/calendar/embed?src=cf4e9c009292b78015417cd0d05ca49ae3e9c3092264b45d2449a299aa1495c0%40group.calendar.google.com&ctz=Asia%2FTokyo" width="800" height="600"></iframe></>
-}
+const Calender: React.FC = () => {
+  console.log(GOOGLE_CALENDER_API_KEY);
+  console.log(GOOGLE_CALENDER_ID);
+  return (
+    <>
+      <iframe
+        src={`https://calendar.google.com/calendar/embed?src=${GOOGLE_CALENDER_ID}&ctz=Asia%2FTokyo`}
+        width="800"
+        height="600"
+      ></iframe>
+    </>
+  );
+};
 
 export default Calender;
